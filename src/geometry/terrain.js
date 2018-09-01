085c81d85d5de252fd3ddbae52a84ba91b11f268
Dave Pagurek <dave@davepagurek.com>
2018-09-01 16:27:01 -0700

    Use Delaunay triangulation

--- src/geometry/terrain.js
+++ src/geometry/terrain.js
@@ -1,20 +1,48 @@
 import { mat4 } from 'gl-matrix';
 import { range } from 'lodash';
+import { Noise } from 'noisejs';
 import qh from 'quickhull3d';
 
-export function terrain(numPoints = 100) {
-  const positions = range(numPoints).map(() => {
-    const x = Math.random() * 2 - 1;
-    const z = Math.random() * 2 - 1;
+const noise = new Noise(Math.random());
+const perlin = (x, y, octaves) => {
+  let val = 0;
+
+  for (let octave = 0; octave < octaves; octave++) {
+    const scale = Math.pow(2, octave);
+    val += noise.perlin2(x * scale, y * scale) / scale;
+  }
+
+  return val;
+};
+
+export function terrain(numPoints = 2000) {
+  const hullPoints = range(numPoints).map(() => {
+    const x = Math.random() * 8 - 4;
+    const y = Math.random() * 8 - 4;
 
     return [
       x,
-      Math.random() * 2 - 1 - 10*(x*x + z*z), // Pull down edges
-      z,
-    ]
+      y,
+      -x*x - y*y
+    ];
   });
 
-  const indices = qh(positions);
+  const positions = hullPoints.map(([a, b, c]) => [
+    a,
+    perlin(a * 1, b * 1, 3)*5, // - 0.1*(a*a + b*b), // Pull down edges
+    b,
+  ]);
+
+  hullPoints.push(
+    [-2000, 0, -4000000],
+    [2000, 2000, -8000000],
+    [2000, -2000, -8000000]
+  );
+
+  const hullIndices = qh(positions);
+
+  const indices =
+    hullIndices.filter(([a, b, c]) => a < positions.length && b < positions.length && c < positions.length);
 
   return {
     positions,
--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ const regl = REGL({ extensions: ['OES_standard_derivatives'] });
 const flat = generateFlat(regl);
 
 const terrainInstance = { ...terrain(), color: [0.2, 0.9, 0.2] };
-mat4.scale(terrainInstance.model, terrainInstance.model, vec3.fromValues(4, 0.5, 4));
-mat4.translate(terrainInstance.model, terrainInstance.model, vec3.fromValues(0, 0, 0));
+mat4.scale(terrainInstance.model, terrainInstance.model, vec3.fromValues(0.5, 0.5, 0.5));
+mat4.translate(terrainInstance.model, terrainInstance.model, vec3.fromValues(0, -4, 0));
 
 const cubeInstance = { ...cube(), color: [0.8, 0.15, 0.15] };
 mat4.scale(cubeInstance.model, cubeInstance.model, vec3.fromValues(0.5, 0.5, 0.5));
@@ -21,7 +21,7 @@ const sun = vec3.fromValues(0, -20, 20);
 const ambient = [0.3, 0.3, 0.3];
 const exposure = 20;
 
-const view = mat4.fromTranslation(mat4.create(), vec3.fromValues(0, -0.3, -3));
+const view = mat4.fromTranslation(mat4.create(), vec3.fromValues(0, -0.3, -5));
 const fieldOfView = Math.PI / 4;
 let width = null;
 let height = null;
